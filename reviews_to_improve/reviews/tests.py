32fe6887666f8dbee92f86d401bbaf997be37d3a
nosrac77 <carson.newton@outlook.com>
2017-12-18 18:55:27 -0800

    wrote one passing test for home view

--- reviews_to_improve/reviews/tests.py
+++ reviews_to_improve/reviews/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class TestHomeView(TestCase):
+    """Class containing home view tests."""
+
+    def setUp(self):
+        """Set up for client creation."""
+        self.client = Client()
+
+    def test_home_view_response_is_200_ok(self):
+        """Function that tests a response from a GET request to the home view
+        url returns an HTTP 200 OK response."""
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
