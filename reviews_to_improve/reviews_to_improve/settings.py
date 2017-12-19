96485a49394313950de134fdf384671da90171e8
nosrac77 <carson.newton@outlook.com>
2017-12-18 16:03:49 -0800

    killed whole env, made new one

--- reviews_to_improve/reviews_to_improve/settings.py
+++ reviews_to_improve/reviews_to_improve/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reviews'
+    'reviews',
+    'storages'
 ]
 
 MIDDLEWARE = [
