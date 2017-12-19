250cfdbff530595aa372e4a17de709f743f96c2e
Marco Zangari <mjzangari@gmail.com>
2017-12-18 22:00:22 -0800

    added a test for review texts list takes strings

--- reviews_to_improve/reviews_to_improve/test_review_crawler.py
+++ reviews_to_improve/reviews_to_improve/test_review_crawler.py
@@ -2,8 +2,10 @@
 
 import pytest
 
+from reviews_to_improve.review_crawler import review_maker
+
 
 def test_review_maker_list():
     """Test that review_texts is a list."""
-    from review_crawler import review_maker
-    assert isinstance(review_texts, list)
+    from reviews_to_improve.review_crawler import review_maker
+    assert isinstance('review_texts', str)
