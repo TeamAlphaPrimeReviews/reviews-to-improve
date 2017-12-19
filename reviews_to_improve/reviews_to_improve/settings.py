b86ec5139b67d8ad7c03e78ddc95f42e01ac4622
nosrac77 <carson.newton@outlook.com>
2017-12-18 16:18:38 -0800

    configured settings

--- reviews_to_improve/reviews_to_improve/settings.py
+++ reviews_to_improve/reviews_to_improve/settings.py
@@ -132,10 +132,10 @@ MEDIAFILES_LOCATION = 'media'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_ACCESS_KEY_ID = os.environ.get('IAM_USER_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = os.environ.get('IAM_USER_SECRET_ACCESS_KEY', '')
-AWS_STORAGE_BUCKET_NAME = 'djangoimagerresources'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_ACCESS_KEY_ID = os.environ.get('IAM_USER_ACCESS_KEY_ID', '')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('IAM_USER_SECRET_ACCESS_KEY', '')
+# AWS_STORAGE_BUCKET_NAME = 'djangoimagerresources'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATICFILES_STORAGE = 'imagersite.custom_storages.StaticStorage'
 
