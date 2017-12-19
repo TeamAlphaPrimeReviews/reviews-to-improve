c195d90309aeef8c90435ec6fff4dd98c6579678
Marco Zangari <mjzangari@gmail.com>
2017-12-18 22:13:52 -0800

    added new test for star_num_list

--- reviews_to_improve/reviews_to_improve/test_review_crawler.py
+++ reviews_to_improve/reviews_to_improve/test_review_crawler.py
@@ -6,6 +6,12 @@ from reviews_to_improve.review_crawler import review_maker
 
 
 def test_review_maker_list():
-    """Test that review_texts is a list."""
+    """Test that review_texts is a list for strings."""
     from reviews_to_improve.review_crawler import review_maker
     assert isinstance('review_texts', str)
+
+
+def test_review_maker_star_num_list():
+    """Test review star num list takes ints."""
+    from reviews_to_improve.review_crawler import review_maker
+    assert isinstance('star_num_list', object)
