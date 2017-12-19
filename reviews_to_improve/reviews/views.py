727ce27e086a93e58d57fc934c1c13910659694a
Marco Zangari <mjzangari@gmail.com>
2017-12-18 18:41:49 -0800

    added new things

--- reviews_to_improve/reviews/views.py
+++ reviews_to_improve/reviews/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request):
+    """View for rendering inital splash page."""
+
+    return render(request, 'reviews/startbootstrap-creative-gh-pages/index.html')
