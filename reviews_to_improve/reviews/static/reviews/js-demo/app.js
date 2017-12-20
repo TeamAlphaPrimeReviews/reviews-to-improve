7e0f19018585a7fe22d1da7bc337c59360193222
nosrac77 <carson.newton@outlook.com>
2017-12-20 15:24:52 -0800

    added alert messages for status updates on load

--- reviews_to_improve/reviews/static/reviews/js-demo/app.js
+++ reviews_to_improve/reviews/static/reviews/js-demo/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $('.progress').hide();
-  $('.alert').hide();
+  $('#green-alert').hide();
+  $('#blue-alert').hide();
 });
 
 $('#load').click(function() {
   $('#load').fadeOut();
+  $('#blue-alert').delay(800).fadeIn().delay(8000).fadeOut();
   $('.progress').delay(800).fadeIn().delay(8000).fadeOut();
-  $('.alert alert-success').fadeIn();
+  $('#green-alert').fadeIn();
 });
