fdc1d84ff30f60f0202d7b9aecb843342f608f24
Marco Zangari <mjzangari@gmail.com>
2017-12-21 15:13:28 -0800

    added setup.py for a better looking tree

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Set up script for python package."""
+
+from setuptools import setup
+
+
+setup(
+    name='review-improve',
+    version='0.7',
+    description='Building better online reviews to empower users',
+    author=['Brendan Davis',
+            'Allan Liebold',
+            'Carson Newton',
+            'Marco Zangari'],
+    author_email='paulrogers@gmail.com',
+    url='https://github.com/TeamAlphaPrimeReviews/reviews-to-improve'
+)
