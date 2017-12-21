d4f2d0fcbd21942f187d2dba4e7a70bd1912d127
Marco Zangari <mjzangari@gmail.com>
2017-12-20 19:43:07 -0800

    one new test on render proper template on about page.

--- reviews_to_improve/reviews/tests.py
+++ reviews_to_improve/reviews/tests.py
@@ -109,3 +109,66 @@ class TestAboutView(TestCase):
         response = self.client.get('/about/')
 
         self.assertNotEqual(response.status_code, 404)
+
+    def test_about_view_renders_proper_template(self):
+        """Test request to about view renders correct template to display on about view."""
+
+        response = self.client.get('/about/')
+
+        self.assertTemplateUsed(response, 'reviews/startbootstrap-full-width-pics-gh-pages/about.html')
+
+    # def test_about_view_not_render_improper_template(self):
+    #     """Test GET request renders NOT the incorrect template."""
+
+    #     response = self.client.get('/')
+
+    #     self.assertTemplateNotUsed(response, 'reviews/startbootstrap-creative-gh-pages/home.html')
+
+    # def test_home_view_response_contains_homepage_elements(self):
+    #     """Function that tests the page rendered contains the elements that the
+    #     home page should contain."""
+
+    #     response = self.client.get('/')
+
+    #     el = '<title>Review Improve</title>'
+    #     self.assertIn(el, str(response.content))
+
+    # def test_home_view_response_converts_properly_to_bytes(self):
+    #     """Test the byte and unicode conversions in the get request."""
+
+    #     response = self.client.get('/')
+
+    #     el = b'<title>Review Improve</title>'
+    #     self.assertIn(el, response.content)
+
+    # def test_home_view_static_files_being_loaded_correctly(self):
+    #     """Function that tests the home view being rendered is also rendering
+    #     the correct static files."""
+
+    #     response = self.client.get('/')
+
+    #     correct_el = '<link href="https://reviews-to-improve-s3.s3.amazonaws.com/static/reviews/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">'
+    #     self.assertIn(correct_el, str(response.content))
+
+    # def test_home_view_static_files_do_not_contain_load_static(self):
+    #     """Function that tests the home view being rendered is also rendering
+    #     the correct static files."""
+
+    #     response = self.client.get('/')
+
+    #     incorrect_el = '<link href="{% static "reviews/vendor/bootstrap/css/bootstrap.min.css" %}" rel="stylesheet">'
+    #     self.assertNotIn(incorrect_el, str(response.content))
+
+    # def test_home_view_contains_word_about(self):
+    #     """Test home view has a tag with about text to make sure still About link."""
+
+    #     response = self.client.get('/').content
+    #     el = 'About'
+    #     self.assertInHTML(el, str(response))
+
+    # def test_home_view_contains_amazon_aws_s3(self):
+    #     """Test home_view contains the string amazon_aws_s3."""
+
+    #     response = self.client.get('/').content
+    #     el = '<script src="https://reviews-to-improve-s3.s3.amazonaws.com/static/reviews/js/creative.min.js"></script>'
+    #     self.assertIn(el, str(response))
