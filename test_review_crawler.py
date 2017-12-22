fdabae32503770844296dba6b961e7d758e83d35
nosrac77 <carson.newton@outlook.com>
2017-12-22 09:39:00 -0800

    made more tests, cleaned up site

--- test_review_crawler.py
+++ test_review_crawler.py
@@ -1,21 +1,35 @@
 """Test objects in review_crawler."""
-
-import pdb
-import pytest
-from django.test import Client
 from review_crawler import review_maker
 
-# from .reviews_to_improve.review_crawler import review_maker
+reviews = review_maker()
 
 
 def test_review_maker_list():
     """Test that review_texts is a list for strings."""
-    assert isinstance('review_texts', str)
+
+    assert isinstance(reviews, set)
 
 
-def test_review_maker_star_num_list():
-    """Test review star num list takes ints."""
-    assert isinstance('star_num_list', object)
+def test_review_maker_lst_contains_instance_of_tuple():
+    """Test that a list version of the set returned from review_maker contains
+    tuples."""
+
+    review_lst = list(reviews)
+    assert isinstance(review_lst[0], tuple)
+
+
+def test_review_maker_lst_has_value_that_can_be_integerized():
+    """Test that the lst of the review set contains a tuple that has a value
+    that is a string representation of an integer."""
+    review_lst = list(reviews)
+    assert isinstance(int(review_lst[0][1]), int)
+
+
+def test_review_maker_lst_has_string_value():
+    """Test that the lst of the review set contains a tuple that has a string
+    value."""
+    review_lst = list(reviews)
+    assert isinstance(str(review_lst[0][0])d, str)
 
 
 # def test_review_maker_all_reviews():
